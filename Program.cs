36013ce1ff277838c7c017f57931d39625c15bcc
Douglas Paz <linodouglas321@gmail.com>
2024-09-30 13:40:08 -0300

    Inicio

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Calculadora_de_Partidas_Rankeadas.models;
+
+string opcao = "";
+
+do
+{
+    Console.Write(@"
+-----------------------------------
+|CALCULADORA DE PARTIDAS RANKEADAS|
+-----------------------------------
+|[1] Adicionar jogador            | 
+|[2] Listar jogadores por nível   |
+|[3] Remover jogador              |
+|[4] Sair                         |
+-----------------------------------
+=> ");
+    opcao = Console.ReadLine();
+
+    Jogador jogador = new Jogador();
+    jogador.Nome = new List<string>();
+    jogador.Saldo = new List<int>();
+
+    if (opcao == "1")
+    {
+        jogador.AdicionarJogador();
+    }
+    else if (opcao == "2")
+    {
+        jogador.ListarJogadoresPorNivel();
+    }
+    else if (opcao == "3")
+    {
+        //RemoverJogador();
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Fim do programa...");
+    }
+
+    //Console.Clear();
+} while (opcao != "4");
---
+++ models/Jogador.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora_de_Partidas_Rankeadas.models
+{
+    public class Jogador
+    {
+        public List<string> Nome { get; set; }
+        public List<int> Saldo { get; set; }
+
+        public void AdicionarJogador()
+        {
+            
+            string nivelJogador = "";
+
+            static int SaldoJogador(int vitorias, int derrotas)
+            {
+                int saldo = vitorias - derrotas;
+                return saldo;
+            }
+
+            Console.Write("Digite o nome do jogador: ");
+            string nomeJogador = Console.ReadLine();
+            Nome.Add(nomeJogador);
+
+            Console.Write("Digite a quantidade de vitórias: ");
+            int vitoriasDoJogador = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de derrotas: ");
+            int derrotasDoJogador = Convert.ToInt32(Console.ReadLine());
+
+            int saldoJogador = SaldoJogador(vitoriasDoJogador, derrotasDoJogador);
+
+            Saldo.Add(saldoJogador);
+
+            if (saldoJogador < 10)
+            {
+                nivelJogador = "Ferro";
+                Console.WriteLine($"{nomeJogador} tem de saldo de {saldoJogador} está no nível {nivelJogador}");
+            }
+            else if (saldoJogador >= 10 && saldoJogador < 20)
+            {
+                nivelJogador = "Bronze";
+                Console.WriteLine($"{nomeJogador} tem de saldo de {saldoJogador} está no nível {nivelJogador}");
+            }
+            else if (saldoJogador >= 20 && saldoJogador < 50)
+            {
+                nivelJogador = "Prata";
+                Console.WriteLine($"{nomeJogador} tem de saldo de {saldoJogador} está no nível {nivelJogador}");
+            }
+            else if (saldoJogador >= 50 && saldoJogador < 80)
+            {
+                nivelJogador = "Ouro";
+                Console.WriteLine($"{nomeJogador} tem de saldo de {saldoJogador} está no nível {nivelJogador}");
+            }
+            else if (saldoJogador >= 80 && saldoJogador < 90)
+            {
+                nivelJogador = "Diamante";
+                Console.WriteLine($"{nomeJogador} tem de saldo de {saldoJogador} está no nível {nivelJogador}");
+            }
+            else if (saldoJogador >= 90 && saldoJogador < 100)
+            {
+                nivelJogador = "Lendário";
+                Console.WriteLine($"{nomeJogador} tem de saldo de {saldoJogador} está no nível {nivelJogador}");
+            }
+            else
+            {
+                nivelJogador = "Imortal";
+                Console.WriteLine($"{nomeJogador} tem de saldo de {saldoJogador} está no nível {nivelJogador}");
+            }
+        }
+
+        public void ListarJogadoresPorNivel()
+        {
+            for (int cont = 0; cont < Nome.Count; cont++)
+            {
+                Console.WriteLine($"{Nome[cont]} - {Saldo[cont]}");
+            }
+        }
+    }
+}
