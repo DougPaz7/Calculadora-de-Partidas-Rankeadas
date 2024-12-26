9618122798b973db4198ad5fc3a5121309c36b87
Douglas Paz <linodouglas321@gmail.com>
2024-12-26 10:15:03 -0300

    .

--- obj/Debug/net8.0/Calculadora-de-Partidas-Rankeadas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculadora-de-Partidas-Rankeadas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora-de-Partidas-Rankeadas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13695ded69b7b8b118d4743f2c4783b0c3fb1933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5257e5ab974cba4d900a571c7890bb7e81880ff2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora-de-Partidas-Rankeadas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora-de-Partidas-Rankeadas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
